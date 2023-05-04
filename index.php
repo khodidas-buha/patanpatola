f1887b1119e967acf6cec2cfef90d81d59c904fc
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-05-04 19:01:35 +0530

    update

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
   </div>
 
 
-  <section class="my-section py-8 px-4 md:py-24 md:px-24 grid grid-cols-1 md:grid-cols-2 gap-8">
+  <!-- <section class="my-section py-8 px-4 md:py-24 md:px-24 grid grid-cols-1 md:grid-cols-2 gap-8">
   <div class="relative">
     <img class="h-auto absolute top-0 left-0" src="src/img/img-1.png" alt="Workplace" width="350" />
     <img class="h-auto cursor:pointer absolute top-0 left-0 md:mt-32 md:ml-40 hover:shadow-outline" src="src/img/img-2.png" width="350" />
@@ -113,7 +113,46 @@
       <img class="absolute top-0 left-0" src="src/img/img-7.png" alt="Workplace" width="350" />
       <img class="cursor:pointer absolute top-0 left-0 mt-32 ml-40 hover:shadow-outline" src="src/img/img-8.png" width="400" />
     </div>
-  </section>
+  </section> -->
+
+<!-- 
+  <section class="my-section py-8 px-4 md:py-24 md:px-24 lg:grid lg:grid-cols-2 gap-8">
+  <div class="relative">
+    <img class="h-auto absolute top-0 left-0" src="src/img/img-1.png" alt="Workplace" width="350" />
+    <img class="h-auto cursor:pointer absolute top-0 left-0 lg:mt-32 lg:ml-40 hover:shadow-outline" src="src/img/img-2.png" width="350" />
+  </div>
+  <div class="container mx-auto max-w-2xl lg:justify-self-end">
+    <h3 class="text-2xl md:text-3xl font-bold mb-4">Title</h3>
+    <p class="text-gray-600 text-base md:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum itaque dolore eveniet ex quod ipsam cum, iusto vitae quae earum?</p>
+  </div>
+</section>
+<section class="my-section py-24 px-4 lg:px-24 lg:grid lg:grid-cols-2 gap-8">
+  <div class="container mx-auto max-w-2xl pt-24 lg:justify-self-start">
+    <h3 class="text-2xl font-bold mb-4">title</h3>
+    <p class="text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum itaque dolore eveniet ex quod ipsam cum, iusto vitae quae earum?</p>
+  </div>
+  <div class="relative">
+    <img class="absolute top-0 left-0" src="src/img/img-3.png" alt="Workplace" width="350" />
+    <img class="cursor:pointer absolute top-0 left-0 lg:mt-32 lg:ml-40 hover:shadow-outline" src="src/img/img-4.png" width="350" />
+  </div>
+</section> -->
+
+<section class="my-section py-8 px-4 lg:py-24 lg:px-24 grid grid-cols-1 md:grid-cols-2 gap-8">
+  <div class="relative">
+    <img class="absolute h-auto top-0 left-0" src="src/img/img-1.png" alt="Workplace" width="350" />
+    <img class="absolute h-auto cursor:pointer top-0 left-0 lg:mt-32 lg:ml-40 hover:shadow-outline" src="src/img/img-2.png" width="350" />
+  </div>
+  <div class="container mx-auto max-w-2xl">
+    <h3 class="text-2xl font-bold mb-4 md:text-3xl">Title</h3>
+    <p class="text-base text-gray-600 md:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum itaque dolore eveniet ex quod ipsam cum, iusto vitae quae earum?</p>
+  </div>
+</section>
+
+
+
+
+
+
 
   <?php include 'footer.php'; ?>
   <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
