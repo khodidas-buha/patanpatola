216f82930e972396d2ccd95094d78185784a2ee3
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-05-04 18:11:33 +0530

    update

--- index.php
+++ index.php
@@ -4,78 +4,121 @@
 <?php include 'head.php'; ?>
 
 <body>
-    <?php include 'header.php'; ?>
+  <?php include 'header.php'; ?>
 
-    <!-- Hero Slider -->
-    <div id="home" class="hero-slider owl-carousel owl-theme absolute">
-  <div class="single-hs-item item-bg1">
-    <div class="d-table">
-      <div class="d-tablecell">
-        <div class="hero-text">
-          <h1>Hero Slider Item 1</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  <!-- Hero Slider -->
+  <div id="home" class="hero-slider owl-carousel owl-theme">
+    <div class="single-hs-item item-bg1">
+      <div class="d-table">
+        <div class="d-tablecell">
+          <div class="hero-text">
+            <h1>Gujrat in Western India is home</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="single-hs-item item-bg2">
+      <div class="d-table">
+        <div class="d-tablecell">
+          <div class="hero-text">
+            <h1>Hero Slider Item 2</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="single-hs-item item-bg3">
+      <div class="d-table">
+        <div class="d-tablecell">
+          <div class="hero-text">
+            <h1>Hero Slider Item 3</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="single-hs-item item-bg4">
+      <div class="d-table">
+        <div class="d-tablecell">
+          <div class="hero-text">
+            <h1>Hero Slider Item 4</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="single-hs-item item-bg5">
+      <div class="d-table">
+        <div class="d-tablecell">
+          <div class="hero-text">
+            <h1>Hero Slider Item 5</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="single-hs-item item-bg6">
+      <div class="d-table">
+        <div class="d-tablecell">
+          <div class="hero-text">
+            <h1>Hero Slider Item 6</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="single-hs-item item-bg2">
-    <div class="d-table">
-      <div class="d-tablecell">
-        <div class="hero-text">
-          <h1>Hero Slider Item 2</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="single-hs-item item-bg3">
-    <div class="d-table">
-      <div class="d-tablecell">
-        <div class="hero-text">
-          <h1>Hero Slider Item 3</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="single-hs-item item-bg4">
-    <div class="d-table">
-      <div class="d-tablecell">
-        <div class="hero-text">
-          <h1>Hero Slider Item 4</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="single-hs-item item-bg5">
-    <div class="d-table">
-      <div class="d-tablecell">
-        <div class="hero-text">
-          <h1>Hero Slider Item 5</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="single-hs-item item-bg6">
-    <div class="d-table">
-      <div class="d-tablecell">
-        <div class="hero-text">
-          <h1>Hero Slider Item 6</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
 
 
+  <section class="my-section py-8 px-4 md:py-24 md:px-24 grid grid-cols-1 md:grid-cols-2 gap-8">
+  <div class="relative">
+    <img class="h-auto absolute top-0 left-0" src="src/img/img-1.png" alt="Workplace" width="350" />
+    <img class="h-auto cursor:pointer absolute top-0 left-0 md:mt-32 md:ml-40 hover:shadow-outline" src="src/img/img-2.png" width="350" />
+  </div>
+  <div class="container mx-auto max-w-2xl">
+    <h3 class="text-2xl md:text-3xl font-bold mb-4">Title</h3>
+    <p class="text-gray-600 text-base md:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum itaque dolore eveniet ex quod ipsam cum, iusto vitae quae earum?</p>
+  </div>
+</section>
 
-    <?php include 'footer.php'; ?>
-    <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'></script>
-<script  src="src/script/script.js"></script>
+  <section class="my-section py-24 px-24 grid grid-cols-1 md:grid-cols-2 gap-8">
+    <div class="container mx-auto max-w-2xl pt-24">
+      <h3 class="text-2xl font-bold mb-4">title</h3>
+      <p class="text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum itaque dolore eveniet ex quod ipsam cum, iusto vitae quae earum?</p>
+    </div>
+    <div class="relative">
+      <img class="absolute top-0 left-0" src="src/img/img-3.png" alt="Workplace" width="350" />
+      <img class="cursor:pointer absolute top-0 left-0 mt-32 ml-40 hover:shadow-outline" src="src/img/img-4.png" width="350" />
+    </div>
+  </section>
+
+  <section class="my-section py-24 px-24 grid grid-cols-1 md:grid-cols-2 gap-8">
+    <div class="relative">
+      <img class="absolute top-0 left-0" src="src/img/img-5.png" alt="Workplace" width="350" />
+      <img class="cursor:pointer absolute top-0 left-0 mt-32 ml-40 hover:shadow-outline" src="src/img/img-6.png" width="350" />
+    </div>
+    <div class="container mx-auto max-w-2xl pt-24">
+      <h3 class="text-2xl font-bold mb-4">title</h3>
+      <p class="text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum itaque dolore eveniet ex quod ipsam cum, iusto vitae quae earum?</p>
+    </div>
+  </section>
+
+  <section class="my-section py-24 px-24 grid grid-cols-1 md:grid-cols-2 gap-8">
+    <div class="container mx-auto max-w-2xl pt-24">
+      <h3 class="text-2xl font-bold mb-4">title</h3>
+      <p class="text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum itaque dolore eveniet ex quod ipsam cum, iusto vitae quae earum?</p>
+    </div>
+    <div class="relative">
+      <img class="absolute top-0 left-0" src="src/img/img-7.png" alt="Workplace" width="350" />
+      <img class="cursor:pointer absolute top-0 left-0 mt-32 ml-40 hover:shadow-outline" src="src/img/img-8.png" width="400" />
+    </div>
+  </section>
+
+  <?php include 'footer.php'; ?>
+  <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'></script>
+  <script src="src/script/script.js"></script>
 </body>
 
 </html>
