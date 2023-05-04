33ce0bca89d7455b156c1e1999a72f6ffd74030e
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-05-04 14:54:31 +0530

    update

---
+++ head.php
@@ -0,0 +1,10 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="src/style/style.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css'>
+</head>
---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>PatanPatola</title>
+<?php include 'head.php'; ?>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <!-- Hero Slider -->
+    <div id="home" class="hero-slider owl-carousel owl-theme absolute">
+  <div class="single-hs-item item-bg1">
+    <div class="d-table">
+      <div class="d-tablecell">
+        <div class="hero-text">
+          <h1>Hero Slider Item 1</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="single-hs-item item-bg2">
+    <div class="d-table">
+      <div class="d-tablecell">
+        <div class="hero-text">
+          <h1>Hero Slider Item 2</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="single-hs-item item-bg3">
+    <div class="d-table">
+      <div class="d-tablecell">
+        <div class="hero-text">
+          <h1>Hero Slider Item 3</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="single-hs-item item-bg4">
+    <div class="d-table">
+      <div class="d-tablecell">
+        <div class="hero-text">
+          <h1>Hero Slider Item 4</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="single-hs-item item-bg5">
+    <div class="d-table">
+      <div class="d-tablecell">
+        <div class="hero-text">
+          <h1>Hero Slider Item 5</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="single-hs-item item-bg6">
+    <div class="d-table">
+      <div class="d-tablecell">
+        <div class="hero-text">
+          <h1>Hero Slider Item 6</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+    <?php include 'footer.php'; ?>
+    <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'></script>
+<script  src="src/script/script.js"></script>
+</body>
+
+</html>
