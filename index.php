12d2c81be70972151af9131f4d7af9f0e577a165
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-05-08 15:02:37 +0530

    update

--- index.php
+++ index.php
@@ -206,46 +206,106 @@
     </div>
   </section>
 
+  <section>
+    <div class="container mx-auto pt-20 pb-8 md:pt-16 md:pb-5 md:pb-0">
+      <div class="text-center">
+        <h2 class="text-red-950 font-bold text-3xl md:text-4xl capitalize mb-4">
+          Sarees
+        </h2>
+        <p class="text-gray-600 text-base md:text-lg text-center mb-4">Patan Patola is a hand-woven saree made
+          from silk in Patan, Gujarat. It features vibrant colors and intricate designs created using a double ikat
+          weaving technique. Each saree takes months to complete and is a prized possession among collectors of Indian
+          textiles.</p>
+      </div>
 
-  <section class="px-4 py-8 md:px-12 md:py-24 lg:px-24 lg:py-32 grid grid-cols-1 lg:grid-cols-2 gap-8">
-    <div class="flex flex-col justify-center items-center lg:items-start">
-      <img class="w-full h-auto mb-8 lg:mb-0 lg:w-96 lg:ml-0 lg:-mt-32 lg:shadow-lg lg:hover:shadow-xl"
-        src="src/img/img-5.png" alt="Workplace" />
-      <img class="w-full h-auto lg:w-96 lg:ml-12 lg:-mt-32 lg:shadow-lg lg:hover:shadow-xl" src="src/img/img-6.png" />
-    </div>
-    <div class="flex flex-col justify-center items-center lg:items-end">
-      <h3 class="text-2xl md:text-3xl font-bold mb-4">Sarees</h3>
-      <p class="text-gray-600 text-base md:text-lg text-center lg:text-right">Patan Patola is a hand-woven saree made
-        from silk in Patan, Gujarat. It features vibrant colors and intricate designs created using a double ikat
-        weaving technique. Each saree takes months to complete and is a prized possession among collectors of Indian
-        textiles.</p>
-    </div>
+      <div class="grid grid-cols-2 md:grid-cols-4 gap-8">
+        <div class="grid gap-4">
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree1.png" alt="">
+          </div>
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree2.png" alt="">
+          </div>
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree3.png" alt="">
+          </div>
+        </div>
+        <div class="grid gap-4">
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree4.png" alt="">
+          </div>
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree5.png" alt="">
+          </div>
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree6.png" alt="">
+          </div>
+        </div>
+        <div class="grid gap-4">
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree7.png" alt="">
+          </div>
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree8.png" alt="">
+          </div>
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree9.png" alt="">
+          </div>
+        </div>
+        <div class="grid gap-4">
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree10.png" alt="">
+          </div>
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree11.png" alt="">
+          </div>
+          <div>
+            <img class="h-auto max-w-full rounded-lg" src="src/img/saree12.png" alt="">
+          </div>
+        </div>
+      </div>
   </section>
-
-
-
-
   
-
-  <section class="px-4 py-8 md:px-12 md:py-24 lg:px-24 lg:py-32 grid grid-cols-1 lg:grid-cols-2 gap-8">
-    <div class="flex flex-col justify-center items-center lg:items-start order-last lg:order-first">
-      <h3 class="text-2xl md:text-3xl font-bold mb-4">Museum</h3>
-      <p class="text-gray-600 text-base md:text-lg text-center lg:text-left">The Sanskrit word tapasya, which roughly
-        translates into penance of visceral intensity and frit through years (decades, even centuries) of meditation,
-        may describe the karmic reality of Patan’s Salvi family .We belong to a community of double Ikat weavers.</p>
-    </div>
-    <div class="flex flex-col justify-center items-center lg:items-end order-first lg:order-last">
-      <img class="w-full h-auto mb-8 lg:mb-0 lg:w-96 lg:mr-0 lg:-mt-32 lg:shadow-lg lg:hover:shadow-xl"
-        src="src/img/img-7.png" alt="Workplace" />
-      <img class="w-full h-auto lg:w-96 lg:mr-12 lg:-mt-32 lg:shadow-lg lg:hover:shadow-xl" src="src/img/img-8.png" />
+  <section class="mt-8" >
+    <div class="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
+      <div
+        class="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
+        <svg class="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block" viewBox="0 0 100 100"
+          fill="currentColor" preserveAspectRatio="none slice">
+          <path d="M50 0H100L50 100H0L50 0Z"></path>
+        </svg>
+        <img class="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+          src="src/img/m1.png" alt="" />
+      </div>
+      <div class="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
+        <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+          <p
+            class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+            Museum
+          </p>
+          <h2 class="mb-5 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+            Sari Crafting: <br class="hidden md:block" />
+            A Historical Journey
+            <span class="inline-block text-deep-purple-accent-400">is real</span>
+          </h2>
+          <p class="pr-5 mb-5 text-base text-gray-700 md:text-lg">
+            Embark on a captivating exploration of the rich heritage and intricate craftsmanship behind sari making.
+            Discover the ancient techniques, vibrant textiles, and cultural significance woven into each fold, as you
+            unravel the fascinating tapestry of Sari Crafting: A Historical Journey.
+          </p>
+          <div class="flex items-center">
+            <a href="/"
+              class="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none">
+              Get started
+            </a>
+            <a href="/" aria-label=""
+              class="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700">Learn
+              more</a>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
-
-
-
-
-
-
 
   <?php include 'footer.php'; ?>
   <script src='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'></script>
@@ -254,6 +314,7 @@
   <script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'></script>
   <script src="src/script/script.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 
 </body>
 
