627739e2580e9ef7fa43a7b589e0b91b2e40c35e
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-05-13 15:05:48 +0530

    update

--- Patola.php
+++ Patola.php
@@ -6,68 +6,62 @@
 <body>
   <?php include 'header.php'; ?>
   <section class="flex flex-col justify-center items-center reletive">
-        <div class="h-30 bg-gray-500 flex justify-center items-center">
-            <img class="max-h-full max-w-full" src="src/img/sareenanner.png" alt="">
-        </div>
-        <div class="h-70 flex justify-center items-center absolute">
-            <p class="text-4xl text-gray-100 font-bold">Sarees</p>
-        </div>
-    </section>
+    <div class="h-30 bg-gray-500 flex justify-center items-center">
+      <img class="max-h-full max-w-full" src="src/img/sareenanner.png" alt="">
+    </div>
+    <div class="h-70 flex justify-center items-center absolute">
+      <p class="text-4xl text-gray-100 font-bold">Sarees</p>
+    </div>
+  </section>
 
-    <section>
-        <div class="h-70 mt-8 flex justify-center items-center">
-            <ol class="inline-flex items-center space-x-1 md:space-x-3">
-                <li aria-current="page">
-                    <div class="inline-flex items-center">
+  <section>
+    <div class="h-70 mt-8 flex justify-center items-center">
+      <ol class="inline-flex items-center space-x-1 md:space-x-3">
+        <li aria-current="page">
+          <div class="inline-flex items-center">
 
-                    </div>
-                    <a href="#"
-                        class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-red-900 dark:text-gray-400 dark:hover:text-white">
-                        <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z">
-                            </path>
-                        </svg>
-                        Home
-                    </a>
-                </li>
-                <li aria-current="page">
-                    <div class="inline-flex items-center">
-                        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                clip-rule="evenodd"></path>
-                        </svg>
-                        <a href="#"
-                            class="ml-1 text-sm font-medium text-gray-700 hover:text-red-900 md:ml-2 dark:text-gray-400 dark:hover:text-white">Sarees</a>
-                    </div>
-                </li>
-            </ol>
-        </div>
-    </section>
+          </div>
+          <a href="#"
+            class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-red-900 dark:text-gray-400 dark:hover:text-white">
+            <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z">
+              </path>
+            </svg>
+            Home
+          </a>
+        </li>
+        <li aria-current="page">
+          <div class="inline-flex items-center">
+            <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"></path>
+            </svg>
+            <a href="#"
+              class="ml-1 text-sm font-medium text-gray-700 hover:text-red-900 md:ml-2 dark:text-gray-400 dark:hover:text-white">Sarees</a>
+          </div>
+        </li>
+      </ol>
+    </div>
+  </section>
 
   <section class="bg-white py-8">
-
     <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
-
       <nav id="store" class="w-full top-0 px-6 py-1">
         <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
-
           <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="#">
             Store
           </a>
-
           <div class="flex items-center" id="store-nav-content">
-
             <a class="pl-3 inline-block no-underline hover:text-black" href="#">
               <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                 viewBox="0 0 24 24">
                 <path d="M7 11H17V13H7zM4 7H20V9H4zM10 15H14V17H10z" />
               </svg>
             </a>
-
             <a class="pl-3 inline-block no-underline hover:text-black" href="#">
               <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                 viewBox="0 0 24 24">
@@ -75,7 +69,6 @@
                   d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z" />
               </svg>
             </a>
-
           </div>
         </div>
       </nav>
@@ -87,9 +80,7 @@
             <p class="">
               Chhabadi 10 Elephant (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -100,9 +91,7 @@
             <p class="">
               Chhabadi Bhat Maroon (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -113,9 +102,7 @@
             <p class="">
               Choktha Bhat (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -126,9 +113,7 @@
             <p class="">
               Elephant In Blue Chhabadi (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -139,9 +124,7 @@
             <p class="">
               Elephant IN Chhabadi (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -152,9 +135,7 @@
             <p class="">
               Lotus Bhat (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -165,9 +146,7 @@
             <p class="">
               Nari Kunjar (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -178,9 +157,7 @@
             <p class="">
               Navratna Blue (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -191,9 +168,7 @@
             <p class="">
               Navratna Green (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -204,9 +179,7 @@
             <p class="">
               Navratna Red (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -215,11 +188,9 @@
           <img class="hover:grow hover:shadow-lg rounded-lg" src="src/img/s11.png">
           <div class="pt-3 flex items-center justify-between">
             <p class="">
-              Navratna Yellow 2 (CH) 
+              Navratna Yellow 2 (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -230,9 +201,7 @@
             <p class="">
               Navratna Yellow 2 (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -243,9 +212,7 @@
             <p class="">
               Sankli Bhat (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -256,9 +223,7 @@
             <p class="">
               Vohra Gaji Blue (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -269,9 +234,7 @@
             <p class="">
               Vohra Gaji Green (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -279,12 +242,10 @@
         <a href="#">
           <img class="hover:grow hover:shadow-lg rounded-lg" src="src/img/s16.png">
           <div class="pt-3 flex items-center justify-between">
-            <p class=""> 
+            <p class="">
               Vohra Gaji Pink (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -294,11 +255,9 @@
           <img class="hover:grow hover:shadow-lg rounded-lg" src="src/img/s17.png">
           <div class="pt-3 flex items-center justify-between">
             <p class="">
-               Yelow Plain Galo (CH)
+              Yelow Plain Galo (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -309,9 +268,7 @@
             <p class="">
               Chanda Bhat (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -322,9 +279,7 @@
             <p class="">
               Chhabdi Bhat (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -335,9 +290,7 @@
             <p class="">
               Choktha Bhat (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -346,11 +299,9 @@
           <img class="hover:grow hover:shadow-lg rounded-lg" src="src/img/s21.png">
           <div class="pt-3 flex items-center justify-between">
             <p class="">
-               Laheriya Bhat (N)
+              Laheriya Bhat (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -359,11 +310,9 @@
           <img class="hover:grow hover:shadow-lg rounded-lg" src="src/img/s22.png">
           <div class="pt-3 flex items-center justify-between">
             <p class="">
-               Navratna Bhat Blue (N)
+              Navratna Bhat Blue (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -374,9 +323,7 @@
             <p class="">
               Navratna Bhat Green (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -387,9 +334,7 @@
             <p class="">
               Paan Bhat (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -400,9 +345,7 @@
             <p class="">
               Phool Bhat (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -413,9 +356,7 @@
             <p class="">
               Sankli Bhat (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -426,9 +367,7 @@
             <p class="">
               Shrikar Bhat (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -437,11 +376,9 @@
           <img class="hover:grow hover:shadow-lg rounded-lg" src="src/img/s28.png">
           <div class="pt-3 flex items-center justify-between">
             <p class="">
-               Vohra Gaji (N)
+              Vohra Gaji (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -452,9 +389,7 @@
             <p class="">
               Yellow Galo With Blocks (N)
             </p>
-            
           </div>
-          
         </a>
       </div>
 
@@ -465,20 +400,14 @@
             <p class="">
               Yelow Plain Galo (CH)
             </p>
-            
           </div>
-          
         </a>
       </div>
-
     </div>
-
   </section>
 
-  <section class="bg-white py-8">
-
+  <!-- <section class="bg-white py-8">
     <div class="container py-8 px-6 mx-auto">
-
     <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl mb-8" href="#">
         About Sarees
       </a>
@@ -495,13 +424,8 @@
         fashion, making them a cherished possession for every discerning woman. Choose the Navratna Green (CH), Shrikar
         Bhat (N), or Navratna Bhat Blue (N) saree from this exquisite collection, and revel in the timeless elegance
         that Patan Patoda has to offer.</p>
-
     </div>
-
-  </section>
-
-
-
+  </section> -->
 
   <?php include 'footer.php'; ?>
 
