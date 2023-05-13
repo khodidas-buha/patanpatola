c6ddd5ae21a39587c4ef3c0869dd39952d91b0c9
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-05-13 15:16:39 +0530

    update

---
+++ Phase2-productlist.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>saree - PatanPatola</title>
+<?php include 'head.php'; ?>
+
+<body>
+  <?php include 'header.php'; ?>
+  <section class="flex flex-col justify-center items-center reletive">
+    <div class="h-30 bg-gray-500 flex justify-center items-center">
+      <img class="max-h-full max-w-full" src="src/img/sareenanner.png" alt="">
+    </div>
+    <div class="h-70 flex justify-center items-center absolute">
+      <p class="text-4xl text-gray-100 font-bold">Sarees</p>
+    </div>
+  </section>
+
+  <section>
+    <div class="h-70 mt-8 flex justify-center items-center">
+      <ol class="inline-flex items-center space-x-1 md:space-x-3">
+        <li aria-current="page">
+          <div class="inline-flex items-center">
+
+          </div>
+          <a href="#"
+            class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-red-900 dark:text-gray-400 dark:hover:text-white">
+            <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z">
+              </path>
+            </svg>
+            Home
+          </a>
+        </li>
+        <li aria-current="page">
+          <div class="inline-flex items-center">
+            <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"></path>
+            </svg>
+            <a href="#"
+              class="ml-1 text-sm font-medium text-gray-700 hover:text-red-900 md:ml-2 dark:text-gray-400 dark:hover:text-white">Sarees</a>
+          </div>
+        </li>
+      </ol>
+    </div>
+  </section>
+
+  
+
+  
+
+  <!-- <section class="bg-white py-8">
+    <div class="container py-8 px-6 mx-auto">
+    <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl mb-8" href="#">
+        About Sarees
+      </a>
+      <p class="mt-8 mb-8">Introducing the exquisite Patan Patoda saree collection, featuring the captivating Navratna
+        Green (CH), Shrikar Bhat (N), and Navratna Bhat Blue (N) saris. Immerse yourself in the rich heritage and
+        timeless beauty of these traditional Indian garments.</p>
+      <p class="mt-8 mb-8">The Navratna Green (CH) saree from the Patan Patoda collection is a true embodiment of
+        elegance and grace. The vibrant green hue is reminiscent of lush emerald gardens, capturing the essence of
+        nature's bounty. Adorned with intricate patterns and delicate motifs, this saree is a masterpiece that showcases
+        the artistry of skilled craftsmen. Crafted with meticulous attention to detail, it exudes a sense of regality
+        and is perfect for special occasions or celebrations.</p>
+      <p class="mt-8 mb-8">Each Patan Patoda saree is a testament to the rich cultural heritage of India, meticulously
+        crafted with a passion for preserving traditional weaving techniques. These saris embody the fusion of art and
+        fashion, making them a cherished possession for every discerning woman. Choose the Navratna Green (CH), Shrikar
+        Bhat (N), or Navratna Bhat Blue (N) saree from this exquisite collection, and revel in the timeless elegance
+        that Patan Patoda has to offer.</p>
+    </div>
+  </section> -->
+
+  <?php include 'footer.php'; ?>
+
+  <script src="src/script/script.js"></script>
+</body>
+
+</html>
