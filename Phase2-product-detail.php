d1ad2e52e1765d8f4afc48220ed947b8bdd3e017
Khodidas Buha <khodidasbuha2001@gmail.com>
2023-05-13 15:32:35 +0530

    update

--- Phase2-product-detail.php
+++ Phase2-product-detail.php
@@ -48,30 +48,155 @@
     </div>
   </section>
 
-  
+  <!--
+  This component uses @tailwindcss/forms and @tailwindcss/typography
 
-  
+  yarn add @tailwindcss/forms @tailwindcss/typography
+  npm install @tailwindcss/forms @tailwindcss/typography
 
-  <!-- <section class="bg-white py-8">
-    <div class="container py-8 px-6 mx-auto">
-    <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl mb-8" href="#">
-        About Sarees
-      </a>
-      <p class="mt-8 mb-8">Introducing the exquisite Patan Patoda saree collection, featuring the captivating Navratna
-        Green (CH), Shrikar Bhat (N), and Navratna Bhat Blue (N) saris. Immerse yourself in the rich heritage and
-        timeless beauty of these traditional Indian garments.</p>
-      <p class="mt-8 mb-8">The Navratna Green (CH) saree from the Patan Patoda collection is a true embodiment of
-        elegance and grace. The vibrant green hue is reminiscent of lush emerald gardens, capturing the essence of
-        nature's bounty. Adorned with intricate patterns and delicate motifs, this saree is a masterpiece that showcases
-        the artistry of skilled craftsmen. Crafted with meticulous attention to detail, it exudes a sense of regality
-        and is perfect for special occasions or celebrations.</p>
-      <p class="mt-8 mb-8">Each Patan Patoda saree is a testament to the rich cultural heritage of India, meticulously
-        crafted with a passion for preserving traditional weaving techniques. These saris embody the fusion of art and
-        fashion, making them a cherished possession for every discerning woman. Choose the Navratna Green (CH), Shrikar
-        Bhat (N), or Navratna Bhat Blue (N) saree from this exquisite collection, and revel in the timeless elegance
-        that Patan Patoda has to offer.</p>
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
+
+  @layer components {
+    .no-spinner {
+      -moz-appearance: textfield;
+    }
+
+    .no-spinner::-webkit-outer-spin-button,
+    .no-spinner::-webkit-inner-spin-button {
+      margin: 0;
+      -webkit-appearance: none;
+    }
+  }
+-->
+
+  <section>
+    <div class="relative mx-auto max-w-screen-xl px-4 py-8">
+      <div class="grid grid-cols-1 items-start gap-8 md:grid-cols-2">
+        <div class="grid grid-cols-2 gap-4 md:grid-cols-1">
+          <img alt="Les Paul"
+            src="src/img/s1.png"
+            class="aspect-square w-full rounded-xl object-cover" />
+        </div>
+
+        <div class="sticky top-0">
+          <strong
+            class="rounded-full border border-blue-600 bg-gray-100 px-3 py-0.5 text-xs font-medium tracking-wide text-blue-600">
+            Pre Order
+          </strong>
+
+          <div class="mt-8 flex justify-between">
+            <div class="max-w-[35ch] space-y-2">
+              <h1 class="text-xl font-bold sm:text-2xl">
+                Fun Product That Does Something Cool
+              </h1>
+
+              <p class="text-sm">Highest Rated Product</p>
+
+              <div class="-ms-0.5 flex">
+                <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+
+                <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+
+                <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+
+                <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+
+                <svg class="h-5 w-5 text-gray-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+              </div>
+            </div>
+
+            <p class="text-lg font-bold">$119.99</p>
+          </div>
+
+          <div class="mt-4">
+            <div class="prose max-w-none">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+                veniam dicta beatae eos ex error culpa delectus rem tenetur,
+                architecto quam nesciunt, dolor veritatis nisi minus inventore,
+                rerum at recusandae?
+              </p>
+            </div>
+
+            <button class="mt-2 text-sm font-medium underline">Read More</button>
+          </div>
+
+          <form class="mt-8">
+            <fieldset>
+              <legend class="mb-1 text-sm font-medium">Color</legend>
+
+              <div class="flex flex-wrap gap-1">
+                <label for="color_tt" class="cursor-pointer">
+                  <input type="radio" name="color" id="color_tt" class="peer sr-only" />
+
+                  <span
+                    class="group inline-block rounded-full border px-3 py-1 text-xs font-medium peer-checked:bg-black peer-checked:text-white">
+                    Texas Tea
+                  </span>
+                </label>
+
+                <label for="color_fr" class="cursor-pointer">
+                  <input type="radio" name="color" id="color_fr" class="peer sr-only" />
+
+                  <span
+                    class="group inline-block rounded-full border px-3 py-1 text-xs font-medium peer-checked:bg-black peer-checked:text-white">
+                    Fiesta Red
+                  </span>
+                </label>
+
+                <label for="color_cb" class="cursor-pointer">
+                  <input type="radio" name="color" id="color_cb" class="peer sr-only" />
+
+                  <span
+                    class="group inline-block rounded-full border px-3 py-1 text-xs font-medium peer-checked:bg-black peer-checked:text-white">
+                    Cobalt Blue
+                  </span>
+                </label>
+              </div>
+            </fieldset>
+
+            <div class="mt-8 flex gap-4">
+              <div>
+                <label for="quantity" class="sr-only">Qty</label>
+
+                <input type="number" id="quantity" min="1" value="1"
+                  class="w-12 rounded border-gray-200 py-3 text-center text-xs [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none" />
+              </div>
+
+              <button type="submit"
+                class="block rounded bg-green-600 px-5 py-3 text-xs font-medium text-white hover:bg-green-500">
+                Add to Cart
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  </section> -->
+  </section>
+
+
+
+
 
   <?php include 'footer.php'; ?>
 
